e6039c63d0dd3411e1b0d95c7b97f3403e00f437
SamiVR-27 <samekshaveerasamy27@gmail.com>
2025-07-05 10:55:10 +0530

    Create Base.java
    need to print in the command line

---
+++ Base.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Base  {
+    public static void main(String[]args){
+       int n=Integer.parseInt(args[0]);
+       int a=Integer.parseInt(args[1]);
+        System.out.println("The Sum of "+ n+" and "+a+" is "+(n+a));
+        }
+    
+}
